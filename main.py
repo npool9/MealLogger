5e54e7e76f0b18fa57a7fcbc1076665efb2a5b9b
Nathan Pool <nathanpool920@gmail.com>
2022-02-02 00:15:24 -0500

    Initialize Meal Logger

---
+++ main.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+class MealLogger:
+    """
+    The controller of the meal logger
+    """
+
+    def __init__(self):
+        """
+        Initialize the Meal Logger controller
+        """
+        print("Meal Logger Initialized.")
+
+
+if __name__ == "__main__":
+    meal_logger = MealLogger()
