ec6f5fbb586bcc6cc3f3c9e5791e7012e397b31f
Nathan Pool <nathanpool920@gmail.com>
2026-02-23 23:12:36 -0500

    fix: fix call to meal log

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def run():
         for ingredient in ingredient_list:
             meal_logger._model.insert_meal_ingredient_bridge(meal, ingredient)
         print("Calculating macros and inserting into meal_log...")
-            meal_logger._model.log_macros(meal, ingredient_list)
+        meal_logger._model.log_macros(meal, ingredient_list, servings_consumed=1)
     else:  # meal exists
         print("Meal exists")
         pass
